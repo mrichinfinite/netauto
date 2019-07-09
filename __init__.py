77157e58c3f1778b7e2b319101386b769786b3f3
vivekksi <vivekksi@cisco.com>
2019-07-09 19:53:50 +0000

    /var/bdb/nas/tasks_dev/aqua_wxm_audio_1 updated by vivekksi

---
+++ __init__.py
@@ -0,0 +1,14 @@
+__copyright__ = "Copyright (c) 2018 Cisco Systems. All rights reserved."
+
+def task(env):
+    """
+    Replace this docstring with your documentation. 
+    
+    This task is run by the bdblib library, full doc and examples at: 
+    https://scripts.cisco.com/doc/
+    Browse more examples in BDB starting with "bdblib_":
+    https://scripts.cisco.com/ui/browse/used/0/bdblib_
+
+    """
+    return 'something useful here'
+    
