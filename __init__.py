db372f12f54edcbed39da053e9d510aa9ea40450
vivekksi <vivekksi@cisco.com>
2019-07-09 19:54:18 +0000

    /var/bdb/nas/tasks_dev/aqua_wxm_audio_1 updated by vivekksi

--- __init__.py
+++ __init__.py
@@ -1,5 +1,12 @@
 __copyright__ = "Copyright (c) 2018 Cisco Systems. All rights reserved."
 
+import json
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 def task(env):
     """
     Replace this docstring with your documentation. 
@@ -10,5 +17,12 @@ def task(env):
     https://scripts.cisco.com/ui/browse/used/0/bdblib_
 
     """
-    return 'something useful here'
+    file_name = os.path.dirname(os.path.realpath(__file__))+"/static_steps.json"
+    logger.info("Steps Json File {}".format(file_name))
+    steps_json= {}
+    with open(file_name,'r') as file_content:
+        steps_json = json.load(file_content)
+    
+    logger.debug("Json retreived from the file {}".format(steps_json))
+    return steps_json
     
