856ddfc143b32f43cbcb59177c23895d90a1c2e3
matrich <matrich@cisco.com>
2019-07-10 20:00:03 +0000

    /var/bdb/nas/tasks_dev/aqua_wxm_audio_1 updated by matrich

---
+++ test_task.py
@@ -0,0 +1,9 @@
+"""
+pytest unit tests
+Functions should start with ``test_`` and contain a list of assertions that evaluate to True if the test succeeds.
+For more info see: http://pytest.org/latest/
+"""
+
+import task_aqua_wxm_audio_1
+def test_example():
+    assert True == True
